2123ca947d1b20d01d05074a37df78426997b9cf
alpaka206 <gyuwon05@gmail.com>
2024-07-18 10:55:24 +0900

    LoadingAnimation

---
+++ src/LoadingAnimation/LoadingAnimation.js
@@ -0,0 +1,33 @@
+import React, { useEffect, useRef, useState } from "react";
+import "./LoadingAnimation.css";
+import image1 from "./images/image1.svg";
+import image2 from "./images/image2.svg";
+import image3 from "./images/image3.svg";
+import image4 from "./images/image4.svg";
+import image5 from "./images/image5.svg";
+import image6 from "./images/image6.svg";
+const imageFiles = [image1, image2, image3, image4, image5, image6];
+
+const LoadingAnimation = () => {
+  const [currentImage, setCurrentImage] = useState(0);
+  const intervalRef = useRef(null);
+
+  useEffect(() => {
+    intervalRef.current = setInterval(() => {
+      setCurrentImage((prevImage) => (prevImage + 1) % imageFiles.length);
+    }, 80); // 0.1초마다 이미지 변경 (fps 조절 가능)
+
+    return () => clearInterval(intervalRef.current);
+  }, []);
+
+  return (
+    <div className="loading-animation">
+      <img
+        src={process.env.PUBLIC_URL + imageFiles[currentImage]}
+        alt="Loading animation"
+      />
+    </div>
+  );
+};
+
+export default LoadingAnimation;
