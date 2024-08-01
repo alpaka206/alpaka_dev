a9a7ac4ff366ed59252b490cddef48e839118af0
alpaka206 <gyuwon05@gmail.com>
2024-08-01 09:26:30 +0900

    KOREA

---
+++ src/KOREA/KOREA.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./KOREA.css";
+
+const KOREA = () => {
+  return (
+    <div className="text-container">
+      <h1 className="text">KOREA</h1>
+    </div>
+  );
+};
+
+export default KOREA;
