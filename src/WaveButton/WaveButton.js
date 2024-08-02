5ad3dcbb7bc81180356a8f21b182eb331e7133f4
alpaka206 <gyuwon05@gmail.com>
2024-08-02 13:25:43 +0900

    WaveButton

---
+++ src/WaveButton/WaveButton.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./WaveButton.css";
+
+const WaveButton = () => {
+  return (
+    <div className="wave-button">
+      <span className="wave"></span>
+      wave
+    </div>
+  );
+};
+
+export default WaveButton;
