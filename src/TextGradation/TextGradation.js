eda09c6b5a92c9b668d6ed8d8e351c65e6e1f71f
alpaka206 <gyuwon05@gmail.com>
2024-10-13 23:38:13 +0900

    TextGradation

---
+++ src/TextGradation/TextGradation.js
@@ -0,0 +1,15 @@
+import React, { useState, useRef, useEffect } from "react";
+import "./TextGradation.css";
+
+const TextGradation = () => {
+  return (
+    <div className="TextGradation">
+      <div className="TextGradation-text">
+        TextGradation 효과입니다. <br />
+        AI가 로딩하는 듯한 효과...?
+      </div>
+    </div>
+  );
+};
+
+export default TextGradation;
